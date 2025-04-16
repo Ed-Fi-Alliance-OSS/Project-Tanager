b7366d9950b55ce20e2a13ef8885d5f12a2d9cf9
Stephen A. Fuqua <stephen@safnet.com>
2025-04-15 21:41:40 -0500

    Create education organizations Call the Python script

--- testing/performance/stuff-and-retrieve-data.ps1
+++ testing/performance/stuff-and-retrieve-data.ps1
@@ -42,14 +42,11 @@ function Invoke-Request {
     return $null
   }
 
-  $location = ""
-  if ($null -ne $responseHeaders["Location"]) {
-    $location = $responseHeaders["Location"]
-  }
-  return @($response, $location)
+  return $response
 }
 
-"Create a Config Service token" | Out-Host
+
+"Create a Management API token" | Out-Host
 $configTokenRequest = Invoke-RestMethod -Uri "http://localhost:$configPort/connect/token" `
   -Method POST `
   -ContentType "application/x-www-form-urlencoded" `
@@ -72,27 +69,44 @@ $vendorResponse = Invoke-Request -Uri "http://localhost:$configPort/v2/vendors" 
   namespacePrefixes   = "uri://ed-fi.org"
 }
 
-$vendorId = $vendorResponse[0].id
+$vendorId = $vendorResponse.id
 
-"Create a new application" | Out-Host
+"Create a new application for managing education organizations" | Out-Host
 $applicationResponse = Invoke-Request -Uri "http://localhost:$configPort/v2/applications" `
   -Method POST `
   -Body @{
-  vendorId        = $vendorId
-  applicationName = "Demo application"
-  claimSetName    = "E2E-NoFurtherAuthRequiredClaimSet"
+  vendorId                 = $vendorId
+  applicationName          = "Education Organizations"
+  claimSetName             = "E2E-RelationshipsWithEdOrgsOnlyClaimSet"
+  educationOrganizationIds = @( 255, 255901 )
 }
 
-$clientKey = $applicationResponse[0].key
-$clientSecret = $applicationResponse[0].secret
+$clientKey = $applicationResponse.key
+$clientSecret = $applicationResponse.secret
 
-# DMS demonstration
-# Read the Token URL from the Discovery API
+
+# Separate application because the built-in claimsets are too limited - the one
+# above can handle ed orgs but not students, and the one below specifically
+# can't handle state education agencies.
+"Create a new application for managing students" | Out-Host
+$applicationResponse = Invoke-Request -Uri "http://localhost:$configPort/v2/applications" `
+  -Method POST `
+  -Body @{
+  vendorId                 = $vendorId
+  applicationName          = "Students"
+  claimSetName             = "E2E-NoFurtherAuthRequiredClaimSet"
+  educationOrganizationIds = @( 255, 255901 )
+}
+
+$studentClientKey = $applicationResponse.key
+$studentClientSecret = $applicationResponse.secret
+
+# Need to create the required descriptors in DMS, which were already in ODS/API
+
 $discoveryResponse = Invoke-RestMethod -Uri "http://localhost:$dmsPort" `
   -Method GET
 
 $tokenUrl = $discoveryResponse[0].urls.oauth
-
 $dataApi = $discoveryResponse[0].urls.dataManagementApi
 
 "Create a DMS token" | Out-Host
@@ -128,13 +142,68 @@ Invoke-Request -Uri "$dataApi/ed-fi/gradeLevelDescriptors" `
   shortDescription = "9th Grade"
 } | Out-Null
 
-"Education organization category descriptors" | Out-Host
+"Education organization category descriptor" | Out-Host
 Invoke-Request -Uri "$dataApi/ed-fi/educationOrganizationCategoryDescriptors" `
   -Method POST `
   -Body @{
   namespace        = "uri://ed-fi.org/EducationOrganizationCategoryDescriptor"
   codeValue        = "School"
   shortDescription = "School"
+} | Out-Null
+Invoke-Request -Uri "$dataApi/ed-fi/educationOrganizationCategoryDescriptors" `
+  -Method POST `
+  -Body @{
+  namespace        = "uri://ed-fi.org/EducationOrganizationCategoryDescriptor"
+  codeValue        = "Local Education Agency"
+  shortDescription = "Local Education Agency"
+} | Out-Null
+Invoke-Request -Uri "$dataApi/ed-fi/educationOrganizationCategoryDescriptors" `
+  -Method POST `
+  -Body @{
+  namespace        = "uri://ed-fi.org/EducationOrganizationCategoryDescriptor"
+  codeValue        = "State Education Agency"
+  shortDescription = "State Education Agency"
+} | Out-Null
+
+"Local education agency category descriptor" | Out-Host
+Invoke-Request -Uri "$dataApi/ed-fi/localEducationAgencyCategoryDescriptors" `
+  -Method POST `
+  -Body @{
+  namespace        = "uri://ed-fi.org/LocalEducationAgencyCategoryDescriptor"
+  codeValue        = "Regular public school district"
+  shortDescription = "Regular public school district"
+} | Out-Null
+
+
+"State Education Agency" | Out-Host
+Invoke-Request -Uri "$dataApi/ed-fi/stateEducationAgencies" `
+  -Method POST `
+  -Body @{
+  stateEducationAgencyId      = 255
+  nameOfInstitution           = "Texas Education Agency"
+  stateAbbreviationDescriptor = "uri://ed-fi.org/StateAbbreviationDescriptor#TX"
+  categories                  = @(
+    @{
+      educationOrganizationCategoryDescriptor = "uri://ed-fi.org/EducationOrganizationCategoryDescriptor#State Education Agency"
+    }
+  )
+} | Out-Null
+
+"Local Education Agency" | Out-Host
+Invoke-Request -Uri "$dataApi/ed-fi/localEducationAgencies" `
+  -Method POST `
+  -Body @{
+  localEducationAgencyId                 = 255901
+  nameOfInstitution                      = "Grand Bend SD"
+  categories                             = @(
+    @{
+      educationOrganizationCategoryDescriptor = "uri://ed-fi.org/EducationOrganizationCategoryDescriptor#Local Education Agency"
+    }
+  )
+  localEducationAgencyCategoryDescriptor = "uri://ed-fi.org/LocalEducationAgencyCategoryDescriptor#Regular public school district"
+  stateEducationAgencyReference          = @{
+    stateEducationAgencyId = 255
+  }
 } | Out-Null
 
 "Create a school" | Out-Host
@@ -155,84 +224,8 @@ Invoke-Request -Uri "$dataApi/ed-fi/schools" `
       gradeLevelDescriptor = "uri://ed-fi.org/GradeLevelDescriptor#Ninth grade"
     }
   )
+  localEducationAgencyReference   = @{localEducationAgencyId = 255901 }
 } | Out-Null
 
-$studentIdList = [System.Collections.ArrayList]::new()
-
-$timed = Measure-Command {
-  # Loop to create students and their education organization associations
-  for ($i = 1; $i -le $studentCount; $i++) {
-    $studentUniqueId = "12345678$i"
-
-    # "Create student $i" | Out-Host
-    $studentResponse = Invoke-Request -Uri "$dataApi/ed-fi/students" `
-      -Method POST `
-      -Body @{
-      studentUniqueId = $studentUniqueId
-      firstName       = "Student$i"
-      lastSurname     = "LastName$i"
-      birthDate       = "2012-01-01"
-    }
-
-    $studentIdList.Add($studentResponse[1]) | Out-Null
-
-    # "Create student education organization association for student $i" | Out-Host
-    Invoke-Request -Uri "$dataApi/ed-fi/studentEducationOrganizationAssociations" `
-      -Method POST `
-      -Body @{
-      studentReference               = @{
-        studentUniqueId = $studentUniqueId
-      }
-      educationOrganizationReference = @{
-        educationOrganizationId = 1
-      }
-      beginDate                      = "2024-08-01"
-      endDate                        = "2025-05-31"
-    } | Out-Null
-  }
-}
-$times = @{
-  "CreateStudents" = $timed.TotalSeconds
-}
-"Created $studentCount students and their education organization associations in $($timed.TotalSeconds) seconds" | Out-Host
-
-$timed = Measure-Command {
-  $studentIdList | ForEach-Object {
-    # "Retrieve student $_" | Out-Host
-    Invoke-Request -Uri $_ -Method GET | Out-Null
-  }
-}
-
-$times["RetrieveStudents"] = $timed.TotalSeconds
-"Retrieved individual student records by ID in $($timed.TotalSeconds) seconds" | Out-Host
-
-
-$timed = Measure-Command {
-  for ($i = 1; $i -le $studentCount; $i++) {
-    $studentUniqueId = "12345678$i"
-
-    # "Retrieve student $i" | Out-Host
-    Invoke-Request -Uri "$dataApi/ed-fi/students?studentUniqueId=$studentUniqueId" `
-      -Method GET | Out-Null
-  }
-}
-
-$times["RetrieveStudentsByQuery"] = $timed.TotalSeconds
-"Retrieved individual student records by query in $($timed.TotalSeconds) seconds" | Out-Host
-
-$timed = Measure-Command {
-  $offset = 0
-
-  do {
-    # "Retrieve students with offset $offset" | Out-Host
-    $students = Invoke-Request -Uri "$dataApi/ed-fi/students?limit=500&offset=$offset" -Method GET
-    # "$dataApi/ed-fi/students?limit=500&offset=$offset", $students.Count | Out-Host
-
-    $offset += 500
-  } while ($students[0].Count -gt 0)
-}
-
-$times["RetrieveAllStudents"] = $timed.TotalSeconds
-"Retrieved all students in $($timed.TotalSeconds) seconds" | Out-Host
-
-$times | ConvertTo-Csv | Out-Host
+# Now run the performance test
+poetry run python odsapi.py --student_count $StudentCount --api_port $DmsPort --client_id $studentClientKey --client_secret $studentClientSecret
